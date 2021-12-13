205e4709f2801eaf864ced65a12ea2116ce09e8d
Florengo <florsg44713@gmail.com>
2021-12-13 18:18:59 -0300

    nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,12 +3,9 @@ import style from "./styles/nav.module.css"
 
 export default function Nav() {
     return (
-        <div className={style.nav}>
-            <div className={style.navBackground}></div>
-            <div>
-                <img src="https://danielmarin.naukas.com/files/2015/09/NASA_seal.svg-1.png" alt="img not found" width="100px" height="100px" />
+        <div className={style.nav}> 
+                <img src="https://danielmarin.naukas.com/files/2015/09/NASA_seal.svg-1.png" alt="img not found" width="70px" height="70px" />
                 <h1>caca</h1>
-            </div>
         </div>
     )
 }
