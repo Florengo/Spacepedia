2d04cd4282509a25709c2930d3a9134fe9e6802c
Florengo <florsg44713@gmail.com>
2021-12-13 18:02:23 -0300

    initial files

---
+++ src/components/nav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./styles/nav.module.css"
+
+export default function Nav() {
+    return (
+        <div className={style.nav}>
+            <div className={style.navBackground}></div>
+            <div>
+                <img src="https://danielmarin.naukas.com/files/2015/09/NASA_seal.svg-1.png" alt="img not found" width="100px" height="100px" />
+                <h1>caca</h1>
+            </div>
+        </div>
+    )
+}
